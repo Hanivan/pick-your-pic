5402253d50b288705e534f94483cd1029baea83e
アニップ <Hanivan@users.noreply.github.com>
2023-01-03 12:58:23 +0700

    fix: footer color

--- components/stateless/Footer.tsx
+++ components/stateless/Footer.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 const Footer: NextPage = () => {
   return (
     <>
-      <footer className="border-t md:flex text-center items-center h-12 mt-5 justify-evenly text-sm">
+      <footer className="border-t border-orange-500 md:flex text-center items-center h-12 mt-5 justify-evenly text-sm">
         <div>
           Hanivan Rizky S. Supported By{" "}
           <Link
